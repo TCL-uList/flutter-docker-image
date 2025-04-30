820d5809ad08a41366b6584d464648423a3883f6
Kaio Delphino <dash.zet@gmail.com>
2025-04-29 21:45:18 -0300

    ci: fix tests

--- script/docker_build_android.sh
+++ script/docker_build_android.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --target android --build-arg flutter_version=3.19.0 --build-arg android_build_tools_version=30.0.3 --build-arg android_platform_versions="28 31 33 34 35" -t android-test .
+docker build --target android --build-arg flutter_version=3.24.5 --build-arg android_build_tools_version=30.0.3 --build-arg android_platform_versions="28 31 33 34" -t android-test .
